e29019eeb8f9e203ad74ab614b44b921b58bcbe5
lelegod <kylenathanya@gmail.com>
2026-03-30 17:34:17 +0200

    cProfile

---
+++ week4/two_loop.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#BSUB -J python
+#BSUB -q hpc
+#BSUB -W 1
+#BSUB -R "rusage[mem=512MB]"
+#BSUB -n 1
+#BSUB -R "span[hosts=1]"
+#BSUB -R "select[model==XeonGold6226R]"
+#BSUB -o python%J.out
+#BSUB -e python_%J.err
+
+source /dtu/projects/02613_2025/conda/conda_init.sh
+conda activate 02613
+
+# python haversine.py /dtu/projects/02613_2025/data/locations/locations_1000.csv
+
+# Autolab
+python -m cProfile -s cumulative haversine.py input.csv
