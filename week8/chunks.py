086560d8dd31cd4370f6ea219586945f366213e9
lelegod <kylenathanya@gmail.com>
2026-05-09 22:05:27 +0200

    fix

--- week8/chunks.py
+++ week8/chunks.py
@@ -1,11 +1,14 @@
 import sys
 import pandas as pd
+import numpy as np
 
 def pandas_chunks(path_name, size):
     df = pd.read_csv(path_name, nrows = size)
-    print(df)
+    print(np.sum(df.value))
 
 if __name__ == '__main__':
-    path_name = str(sys.argv(1))
-    size = int(sys.argv(2))
-    pandas_chunks()
+    # path_name = str(sys.argv[1])
+    # size = int(sys.argv[2])
+    path_name = "/dtu/projects/02613_2025/data/dmi/2023_01.csv.zip"
+    size = 5
+    pandas_chunks(path_name, size)
