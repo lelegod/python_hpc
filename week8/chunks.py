903e573253316c0817a50bfea97a51f78cd661ed
lelegod <2021m622@kuas.ac.jp>
2026-05-09 22:08:22 +0200

    fix again

--- week8/chunks.py
+++ week8/chunks.py
@@ -3,12 +3,12 @@ import pandas as pd
 import numpy as np
 
 def pandas_chunks(path_name, size):
-    df = pd.read_csv(path_name, nrows = size)
-    print(np.sum(df.value))
+    total = 0
+    for chunk in pd.read_csv(path_name, chunksize=size):
+        total += chunk.loc[chunk['parameterId'] == 'precip_past10min', 'value'].sum()
+    print(total)
 
 if __name__ == '__main__':
-    # path_name = str(sys.argv[1])
-    # size = int(sys.argv[2])
-    path_name = "/dtu/projects/02613_2025/data/dmi/2023_01.csv.zip"
-    size = 5
+    path_name = str(sys.argv[1])
+    size = int(sys.argv[2])
     pandas_chunks(path_name, size)
